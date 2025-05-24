27613ea2effe6819e325a2522eb892e1eabf81fd
thanhhuy1234ok <nguyenvothanhhuy2002@gmail.com>
2025-05-24 14:21:46 +0700

    init commit

---
+++ src/model/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+
+export default mongoose.model('User', userSchema);
---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cors = require('cors');
+const port = process.env.PORT || 8000;
+const router = require('./router/index.js');
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/myapp';
+
+const connectDB = async () => {
+    try {
+        console.log('mongoURI', mongoURI);
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
+
+connectDB();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', router);
+
+app.listen(port, () => {
+    console.log(`Server started on port http://localhost:${port}`);
+}
+);
